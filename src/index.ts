3289a3ee9d247587f30292a6dc83654a02b22772
Daniel Imhoff <dwieeb@gmail.com>
2018-06-27 15:55:40 -0500

    refactor: tslint does not use these exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-export * from 'codelyzer/angular/config';
-export { Rule as ActionSheetTitleAndSubtitleAreNowHeaderAndSubheader } from './ionActionSheetTitleAndSubtitleAreNowHeaderAndSubHeaderRule';
-export { Rule as AlertTitleAndSubtitleAreNowHeaderAndSubheader } from './ionAlertTitleAndSubtitleAreNowHeaderAndSubHeaderRule';
-export { Rule as IonButtonAttributesAreRenamedRule } from './ionButtonAttributesAreRenamedRule';
-export { Rule as IonButtonIsNowAnElementRule } from './ionButtonIsNowAnElementRule';
-export { Rule as IonChipMarkupChangedRule } from './ionChipMarkupHasChangedRule';
-export { Rule as IonNavbarIsNowIonToolbarRule } from './ionNavbarIsNowIonToolbarRule';
-export { Rule as IonTabBadgeStyleIsNowBadgeStyle } from './ionTabBadgeStyleIsNowBadgeStyleRule';
-export { Rule as IonTabIconIsNowIcon } from './ionTabIconIsNowIconRule';
-export { Rule as IonTabBadgeIsNowBadge, Rule as IonTabTitleIsNowLabel } from './ionTabTitleIsNowLabelRule';
-
 // this file exists for tslint to resolve the rules directory
 export const rulesDirectory = '.';
