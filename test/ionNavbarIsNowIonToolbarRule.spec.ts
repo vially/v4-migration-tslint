c13764fa30d7ae10c2fb5419bf03997848d271f0
dwieeb <dwieeb@gmail.com>
2018-07-07 00:01:59 -0500

    fix(rules): uncombine ion-navbar-is-now-ion-toolbar rule (#38)
    Previously the rule was adding an ion-back-button (and container) as
    well as changing the tag names to ion-toolbar.
    
    We have a rule for warning about the missing back button: `ion-back-button-not-added-by-default`
    
    It's probably best to keep these separated for simplicity

--- test/ionNavbarIsNowIonToolbarRule.spec.ts
+++ test/ionNavbarIsNowIonToolbarRule.spec.ts
@@ -19,196 +19,63 @@ describe(ruleName, () => {
   describe('failure', () => {
     it('should fail when navbar is passed in', () => {
       let source = `
-            @Component({
-              template: \` <ion-navbar></ion-navbar>
-                                              ~~~~~~~~~~~~~~~~~
-              \`
-            })
-            class Bar {}
-          `;
+        @Component({
+          template: \`
+            <ion-navbar></ion-navbar>
+             ~~~~~~~~~~
+          \`
+        })
+        class Bar {}
+      `;
 
-      const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
-        startPosition: {
-          line: 2,
-          character: 27
-        },
-        endPosition: {
-          line: 2,
-          character: 39
-        }
-      };
-
-      assertFailure(ruleName, source, fail);
+      assertAnnotated({
+        ruleName,
+        source,
+        message: 'The ion-navbar component is now named ion-toolbar.'
+      });
     });
 
     it('should fail when navbar is passed in', () => {
       let source = `
-            @Component({
-              template: \` 
-              <ion-navbar>
-                <label>Something</label>
-              </ion-navbar>
-              \`
-            })
-            class Bar {}
-          `;
+        @Component({
+          template: \`
+          <ion-navbar>
+           ~~~~~~~~~~
+            <label>Something</label>
+          </ion-navbar>
+          \`
+        })
+        class Bar {}
+      `;
 
-      const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
-        startPosition: {
-          line: 3,
-          character: 15
-        },
-        endPosition: {
-          line: 4,
-          character: 0
-        }
-      };
-
-      assertFailure(ruleName, source, fail);
+      assertAnnotated({
+        ruleName,
+        source,
+        message: 'The ion-navbar component is now named ion-toolbar.'
+      });
     });
   });
 
   describe('replacements', () => {
-    it('should fail when navbar is passed in', () => {
+    it('should fail when ion-navbar is used', () => {
       let source = `
-            @Component({
-              template: \` <ion-navbar></ion-navbar>
-              \`
-            })
-            class Bar {}
-          `;
+        @Component({
+          template: \`
+            <ion-navbar></ion-navbar>
+          \`
+        })
+        class Bar {}
+      `;
 
       const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
-        startPosition: {
-          line: 2,
-          character: 27
-        },
-        endPosition: {
-          line: 2,
-          character: 39
-        }
-      };
-
-      const failures = assertFailure(ruleName, source, fail);
-      const fixes = failures.map(f => f.getFix());
-      const res = Replacement.applyAll(source, Utils.flatMap(fixes, Utils.arrayify));
-
-      expect(res).to.eq(`
-            @Component({
-              template: \` <ion-toolbar>
-  <ion-buttons slot="start">
-    <ion-back-button></ion-back-button>
-  </ion-buttons>
-</ion-toolbar>
-              \`
-            })
-            class Bar {}
-          `);
-    });
-
-    it('should fail when navbar is passed in', () => {
-      let source = `
-            @Component({
-              template: \` <ion-navbar><label>Hello</label></ion-navbar>
-              \`
-            })
-            class Bar {}
-          `;
-
-      const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
-        startPosition: {
-          line: 2,
-          character: 27
-        },
-        endPosition: {
-          line: 2,
-          character: 39
-        }
-      };
-
-      const failures = assertFailure(ruleName, source, fail);
-      const fixes = failures.map(f => f.getFix());
-      const res = Replacement.applyAll(source, Utils.flatMap(fixes, Utils.arrayify));
-
-      expect(res).to.eq(`
-            @Component({
-              template: \` <ion-toolbar>
-  <ion-buttons slot="start">
-    <ion-back-button></ion-back-button>
-  </ion-buttons><label>Hello</label>
-</ion-toolbar>
-              \`
-            })
-            class Bar {}
-          `);
-    });
-
-    it('should fail when navbar is passed in', () => {
-      let source = `
-            @Component({
-              template: \`
-              <ion-navbar>
-                <ion-title>Edit Group</ion-title>
-              </ion-navbar>
-              \`
-            })
-            class Bar {}
-          `;
-
-      const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
+        message: 'The ion-navbar component is now named ion-toolbar.',
         startPosition: {
           line: 3,
-          character: 15
-        },
-        endPosition: {
-          line: 4,
-          character: 0
-        }
-      };
-
-      const failures = assertFailure(ruleName, source, fail);
-      const fixes = failures.map(f => f.getFix());
-      const res = Replacement.applyAll(source, Utils.flatMap(fixes, Utils.arrayify));
-
-      expect(res).to.eq(`
-            @Component({
-              template: \`
-              <ion-toolbar>
-  <ion-buttons slot="start">
-    <ion-back-button></ion-back-button>
-  </ion-buttons><ion-title>Edit Group</ion-title>
-</ion-toolbar>              \`
-            })
-            class Bar {}
-          `);
-    });
-
-    it('should fail when navbar is passed in', () => {
-      let source = `
-            @Component({
-              template: \`
-              <ion-navbar attr="something">
-                <ion-title>Edit Group</ion-title>
-              </ion-navbar>
-              \`
-            })
-            class Bar {}
-          `;
-
-      const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
-        startPosition: {
-          line: 3,
-          character: 14
+          character: 13
         },
         endPosition: {
           line: 3,
-          character: 43
+          character: 23
         }
       };
 
@@ -217,48 +84,36 @@ describe(ruleName, () => {
       const res = Replacement.applyAll(source, Utils.flatMap(fixes, Utils.arrayify));
 
       expect(res).to.eq(`
-            @Component({
-              template: \`
-             <ion-toolbar>
-  <ion-buttons slot="start">
-    <ion-back-button></ion-back-button>
-  </ion-buttons><ion-title>Edit Group</ion-title>
-</ion-toolbar>
-              \`
-            })
-            class Bar {}
-          `);
+        @Component({
+          template: \`
+            <ion-toolbar></ion-toolbar>
+          \`
+        })
+        class Bar {}
+      `);
     });
 
-    it('should fail when navbar is passed in', () => {
+    it('should fail when ion-navbar is used on multiple lines', () => {
       let source = `
-            @Component({
-              template: \`
-              <ion-header>
-                <ion-navbar>
-                  <ion-button menuToggle>
-                    <ion-icon name="menu"></ion-icon>
-                  </ion-button>
-                  <ion-title>Profile</ion-title>
-                </ion-navbar>
-              </ion-header>
-              <ion-content>
-                <user-profile></user-profile>
-              </ion-content>
-              \`
-            })
-            class Bar {}
-          `;
+        @Component({
+          template: \`
+            <ion-navbar>
+              <ion-title>My Navigation Bar</ion-title>
+            </ion-navbar>
+          \`
+        })
+        class Bar {}
+      `;
 
       const fail = {
-        message: 'ion-navbar is no longer used. Please use ion-toolbar.',
+        message: 'The ion-navbar component is now named ion-toolbar.',
         startPosition: {
-          line: 4,
-          character: 17
+          line: 3,
+          character: 13
         },
         endPosition: {
-          line: 5,
-          character: 0
+          line: 3,
+          character: 23
         }
       };
 
@@ -267,24 +122,15 @@ describe(ruleName, () => {
       const res = Replacement.applyAll(source, Utils.flatMap(fixes, Utils.arrayify));
 
       expect(res).to.eq(`
-            @Component({
-              template: \`
-              <ion-header>
-                <ion-toolbar>
-  <ion-buttons slot="start">
-    <ion-back-button></ion-back-button>
-  </ion-buttons><ion-button menuToggle>
-                    <ion-icon name="menu"></ion-icon>
-                  </ion-button>
-                  <ion-title>Profile</ion-title>
-</ion-toolbar>
-              <ion-content>
-                <user-profile></user-profile>
-              </ion-content>
-              \`
-            })
-            class Bar {}
-          `);
+        @Component({
+          template: \`
+            <ion-toolbar>
+              <ion-title>My Navigation Bar</ion-title>
+            </ion-toolbar>
+          \`
+        })
+        class Bar {}
+      `);
     });
   });
 });
